8931bed21ab51831e27d9a9766c96c6fc4ef2e4c
LHcouto <xluisx_couto@hotmail.com>
2022-05-03 23:52:23 -0300

    feat:estilizando modal adicionar

--- src/components/AdicionaPaletaModal/AdicionaPaletaModal.jsx
+++ src/components/AdicionaPaletaModal/AdicionaPaletaModal.jsx
@@ -1,81 +1,82 @@
-import { useState } from 'react';
-import Modal from 'components/Modal/Modal';
-import './AdicionaPaletaModal.css';
+import { useState } from "react";
+import Modal from "components/Modal/Modal";
+import './AdicionaPaletaModal.css'
 
-function AdicionaPaletaModal({closeModal}){
+function AdicionaPaletaModal({ closeModal }) {
     const form = {
-        preco:"",
-        sabor:"",
-        recheio:"",
-        descricao:"",
-        foto:"",
-    }
+        preco: "",
+        sabor: "",
+        recheio: "",
+        descricao: "",
+        foto: "",
+    };
+
     const [state, setState] = useState(form);
 
     const handleChange = (e, name) => {
-        setState({...state, [name]: e.target.value})
+        setState({ ...state, [name]: e.target.value, });
+    };
 
-    }
-    return(
-        <Modal closeModal ={closeModal}>
+    return (
+        <Modal closeModal={closeModal}>
             <div className="AdicionaPaletaModal">
-                <form autoComplete ="false">
-                    <h2>Adicionar ao Cardápio</h2>
+                <form autoComplete="off">
+                    <h2> Adicionar ao Cardápio </h2>
                     <div>
-                        <label className="AdicionaPaletaModal__text" htmlFor="preco">Preço:</label>
-                        <input 
-                        id="preco" 
-                        type="text" 
-                        placeholder='R$ 10,00'
-                        value={state.preco}
-                        onChange={(e) => handleChange(e, "preco")}/>
-                    </div>
-                    <div>
-                    <label className="AdicionaPaletaModal__text" htmlFor="sabor">Sabor:</label>
-                        <input 
-                        id="sabor" 
-                        type="text" 
-                        placeholder='Chocolate'
-                        value={state.sabor}
-                        onChange={(e) => handleChange(e, "sabor")}/>
-                    </div>
-                    <div>
-                    <label className="AdicionaPaletaModal__text" htmlFor="recheio">Recheio:</label>
-                        <input 
-                        id="recheio" 
-                        type="text" 
-                        placeholder='banana'
-                        value={state.recheio}
-                        onChange={(e) => handleChange(e, "recheio")}/>
-                    </div>
-                    <div>
-                    <label className="AdicionaPaletaModal__text" htmlFor="descricao">Descrição:</label>
-                        <input 
-                        id="descricao" 
-                        type="text" 
-                        placeholder='Detalho o produto'
-                        value={state.descricao}
-                        onChange={(e) => handleChange(e, "descricao")}/>
-                    </div>
-                    <div>
-                    <label className="AdicionaPaletaModal__text AdicionaPAletaModal__foto-label" htmlFor="foto">
-                    {!state.foto.length ? "Selecionar Imagem":state.foto}
-                    </label>
+                        <label className="AdicionaPaletaModal__text" htmlFor="preco"> Preco: </label>
                         <input
-                        className="AdicionaPaletaModal__foto" 
-                        id="foto" 
-                        type="file"
-                        accept='image/png, image/gif, image/jpeg' 
-                        value={state.foto}
-                        onChange={(e) => handleChange(e, "foto")}/>
+                            id="preco"
+                            placeholder="10,00"
+                            type="text"
+                            value={state.preco}
+                            onChange={(e) => handleChange(e, "preco")} />
                     </div>
+                    <div>
+                        <label className="AdicionaPaletaModal__text" htmlFor="sabor"> Sabor: </label>
+                        <input
+                            id="sabor"
+                            placeholder="Chocolate"
+                            type="text"
+                            value={state.sabor}
+                            onChange={(e) => handleChange(e, "sabor")} />
+                    </div>
+                    <div>
+                        <label className="AdicionaPaletaModal__text" htmlFor="recheio"> Recheio: </label>
+                        <input
+                            id="recheio"
+                            placeholder="Banana"
+                            type="text"
+                            value={state.recheio}
+                            onChange={(e) => handleChange(e, "recheio")} />
+                    </div>
+                    <div>
+                        <label className="AdicionaPaletaModal__text" htmlFor="descricao"> Descricao: </label>
+                        <input
+                            id="descricao"
+                            placeholder="Detalhe o produto"
+                            type="text"
+                            value={state.descricao}
+                            onChange={(e) => handleChange(e, "descricao")} />
+                    </div>
+                    <div>
+                        <label className="AdicionaPaletaModal__text  AdicionaPaletaModal__foto-label" htmlFor="foto" >
+                            {!state.foto.length ? "Selecionar Imagem" : state.foto}
+                        </label>
+                        <input
+                            className=" AdicionaPaletaModal__foto"
+                            id="foto"
+                            type="file"
+                            accept="image/png, image/gif, image/jpeg"
+                            value={state.foto}
+                            onChange={(e) => handleChange(e, "foto")} />
+                    </div>
+
                     <input
-                    className="AdicionaPaletaModal__enviar"
-                    type="submit"
-                    value="Enviar"/>
+                        className="AdicionaPaletaModal__enviar"
+                        type="submit"
+                        value="Enviar" />
                 </form>
             </div>
-
         </Modal>
     );
 }
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,14 +1,20 @@
-import PaletaLista from 'components/PaletaLista/PaletaLista';
-import Navbar from 'components/Navbar/Navbar';
-import AdicionaPaletaModal from 'components/AdicionaPaletaModal/AdicionaPaletaModal';
 import './Home.css';
+import PaletaLista from 'components/PaletaLista/PaletaLista';
+import AdicionaPaletaModal from 'components/AdicionaPaletaModal/AdicionaPaletaModal';
+import Navbar from 'components/Navbar/Navbar';
+import { useState } from 'react';
 
 function Home() {
+  const [canShowAdicionaPaletaModal, setCanShowAdicionaPaletaModal] = useState(false);
+
   return <div className="Home">
-      <Navbar />
+      <Navbar createPaleta={() => setCanShowAdicionaPaletaModal(true)} />
+
       <div className="Home__container">
         <PaletaLista />
-        <AdicionaPaletaModal />
+        {
+            canShowAdicionaPaletaModal && (<AdicionaPaletaModal closeModal={() => setCanShowAdicionaPaletaModal(false)}/>)
+        }
       </div>
     </div>
   
